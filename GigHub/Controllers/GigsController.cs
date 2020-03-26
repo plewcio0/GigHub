04af2d9170826731e13484fd1f59a1dc24e48b24
Daniel <plewa.daniel0@gmail.com>
2020-03-26 13:28:15 +0100

    Added GigFormViewModel, set context in GigsController, passed viewModel to View

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,13 +1,25 @@
-﻿using System.Web.Mvc;
+﻿using GigHub.Models;
+using GigHub.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace GigHub.Controllers
 {
     public class GigsController : Controller
     {
+        private ApplicationDbContext _context;
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
         // GET: Gigs
         public ActionResult Create()
         {
-            return View();
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
         }
     }
 }
