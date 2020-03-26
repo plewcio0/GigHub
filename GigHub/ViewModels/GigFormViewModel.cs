2b14f2396bc0e4c15e415016accb2a2392bae3c6
Daniel <plewa.daniel0@gmail.com>
2020-03-26 18:31:35 +0100

    Add custom validation to Date and Time in GigFormViewModel.

--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -11,13 +11,16 @@ namespace GigHub.ViewModels
         public string Venue { get; set; }
 
         [Required]
+        [FutureDate]
         public string Date { get; set; }
 
         [Required]
+        [ValidTime]
         public string Time { get; set; }
 
         [Required]
         public byte Genre { get; set; }
+
         public IEnumerable<Genre> Genres { get; set; }
         public DateTime GetDateTime()
         {
