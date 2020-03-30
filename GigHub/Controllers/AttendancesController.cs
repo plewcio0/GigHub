3b551507534c341a7f9f35a6eb307bc4b2e815c9
Daniel <plewa.daniel0@gmail.com>
2020-03-30 11:33:12 +0200

    Added Attendance API.

---
+++ GigHub/Controllers/AttendancesController.cs
@@ -0,0 +1,38 @@
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody]int gigId)
+        {
+            var userId = User.Identity.GetUserId();
+            var exists = _context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == gigId);
+            if(!exists)
+            {
+                var attendance = new Attendance
+                {
+                    GigId = gigId,
+                    AttendeeId = userId
+                };
+                _context.Attendances.Add(attendance);
+                _context.SaveChanges();
+            }
+            else
+            {
+                return BadRequest("The attendance already exist.");
+            }
+            return Ok();
+        }
+    }
+}
