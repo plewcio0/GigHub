ebf0938e52714adbd9a866ddb433945d7e85baaa
Daniel <plewa.daniel0@gmail.com>
2020-05-08 10:47:27 +0200

    Js toggleAttendances refractoring.

--- GigHub/App_Start/BundleConfig.cs
+++ GigHub/App_Start/BundleConfig.cs
@@ -7,6 +7,9 @@ namespace GigHub
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                        "~/Scripts/app/app.js"
+                ));
             bundles.Add(new ScriptBundle("~/bundles/lib").Include(
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/bootstrap.js",
---
+++ GigHub/Scripts/app/app.js
@@ -0,0 +1,49 @@
+﻿var GigsController = function () {
+    var button;
+
+    var init = function () {
+        $('.js-toggle-attendance').click(toggleAttendance);
+    };
+
+
+    var toggleAttendance = function (e) {
+        button = $(e.target);
+        if (button.hasClass("btn-default")) {
+            createAttendance();
+        }
+        else {
+            deleteAttendance();
+        }
+
+    };
+
+    var createAttendance = function () {
+        $.post("/api/attendances", { gigId: button.attr("data-gig-id") })
+            .done(done)
+            .fail(fail);
+    };
+
+    var deleteAttendance = function () {
+        $.ajax({
+            url: "/api/attendances/" + button.attr("data-gig-id"),
+            method: "delete"
+        })
+            .done(done)
+            .fail(fail);
+    };
+    var done = function () {
+        var text = (button.text() == "Going" ? "Going?" : "Going");
+        button.toggleClass("btn-info").toggleClass("btn-default").text(text);
+        if (text == "Going")
+            alertify.success('You are going to this event.').dismissOthers();
+        else
+            alertify.warning('You are not going to this event.').dismissOthers();
+    }
+    var fail = function (xhr) {
+        alertify.alert(xhr.responseJSON.message);
+    };
+
+    return {
+        init: init
+    }
+}();
